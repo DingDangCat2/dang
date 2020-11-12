ef76d8ebaf6ed8bd921b1b38341b3852f7f58e1b
xjf <xjf1504654420@163.com>
2020-11-12 23:33:55 +0800

    03-js

---
+++ 03-js/03.js
@@ -0,0 +1,12 @@
+/* var num1=22;
+var num2=~num1;
+console.log(num2); */ 
+
+/* console.log(Infinity*"hgfj") */
+
+/* console.log(0/5) */
+/* console.log(5%0) */
+/* console.log(NaN/Infinity); */
+/* console.log(NaN/0); */
+/* console.log(NaN%0); */
+/* console.log(Infinity/0); */
